04831dc7eedfa997cafc107159355eca53db9c19
channdara <channdara@mango-byte.com>
2021-01-18 12:35:13 +0700

    add request api and mvp

--- app/src/main/java/com/example/basic_application/network/api/NetworkConfig.kt
+++ app/src/main/java/com/example/basic_application/network/api/NetworkConfig.kt
@@ -1,20 +1,33 @@
 package com.example.basic_application.network.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class NetworkConfig {
+class NetworkConfig(private val baseURl: String = "https://jsonplaceholder.typicode.com/") {
     private var instance: Retrofit? = null
 
     fun retrofit(): Retrofit {
         if (instance == null) {
             instance = Retrofit.Builder()
-                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .baseUrl(baseURl)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
         }
         return instance!!
     }
+
+    private val okHttpClient: OkHttpClient
+        get() = OkHttpClient()
+            .newBuilder()
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder().apply {
+                    header("Authorization", "")
+                }.build()
+                chain.proceed(request)
+            }
+            .build()
 }
